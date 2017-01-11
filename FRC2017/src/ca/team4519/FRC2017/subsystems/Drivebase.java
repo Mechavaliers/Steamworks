370520d642e2bcf1080e07f76598e9936f7eb8c9
Connor Adams <connor.adams@uoit.net>
2017-01-11 11:43:44 -0500

    Began construction drivebase subclass

--- FRC2017/src/ca/team4519/FRC2017/subsystems/Drivebase.java
+++ FRC2017/src/ca/team4519/FRC2017/subsystems/Drivebase.java
@@ -1,5 +1,33 @@
 package ca.team4519.FRC2017.subsystems;
 
+import ca.team4519.FRC2017.Constants;
+
+import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj.Encoder;
+
 public class Drivebase {
 
+	VictorSP leftDriveA;
+	VictorSP leftDriveB;
+	VictorSP rightDriveA;
+	VictorSP rightDriveB;
+
+	Encoder leftDriveEncodder;
+	Encoder rightDriveEncoder;
+	
+	public void drivebaseInit() {
+		leftDriveA = new VictorSP(Constants.leftDriveA);
+		leftDriveB = new VictorSP(Constants.leftDriveB);
+		rightDriveA = new VictorSP(Constants.rightDriveA);
+		rightDriveB = new VictorSP(Constants.rightDriveB);
+		
+		leftDriveEncodder = new Encoder(Constants.leftDriveEncoderA, Constants.leftDriveEncoderB, false);
+		rightDriveEncoder = new Encoder(Constants.rightDriveEncoderA, Constants.rightDriveEncoderB, false);
+	}
+	
+	public void resetState() {
+		
+	}
+	
+	
 }
