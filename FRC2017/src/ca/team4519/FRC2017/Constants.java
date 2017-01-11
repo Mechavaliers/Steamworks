22dee41c131144831e8af305903431d97705d837
Connor <connor.adams@uoit.net>
2017-01-10 20:54:56 -0500

    Constant edits

--- FRC2017/src/ca/team4519/FRC2017/Constants.java
+++ FRC2017/src/ca/team4519/FRC2017/Constants.java
@@ -6,6 +6,9 @@ public class Constants {
 	 * AS OF INITAL COMMIT ALL VALUES ARE 0 AND ONLY ACT AS PLACEHOLDERS
 	 */
 
+	//Controlls
+	public static final int gamepad = 0;
+	
 	//Drivebase-Motors
 	public static final int LeftDriveA = 0;
 	public static final int LeftDriveB = 0;
@@ -17,5 +20,7 @@ public class Constants {
 	public static final int LeftDriveEncoderB = 0;
 	public static final int RightDriveEncoderA = 0;
 	public static final int RightDriveEncoderB = 0;
+	
+	
 	
 }
