9c7781fa0e3d7c235b7a534a15e69d57de2c241b
Connor <ceeman.adams@gmail.com>
2017-01-10 20:38:34 -0500

    Created Constants file

---
+++ FRC2017/src/ca/team4519/FRC2017/Constants.java
@@ -0,0 +1,21 @@
+package ca.team4519.FRC2017;
+
+public class Constants {
+
+	/*
+	 * AS OF INITAL COMMIT ALL VALUES ARE 0 AND ONLY ACT AS PLACEHOLDERS
+	 */
+
+	//Drivebase-Motors
+	public static final int LeftDriveA = 0;
+	public static final int LeftDriveB = 0;
+	public static final int RightDriveA = 0;
+	public static final int RightDriveB = 0;
+	
+	//Drivebase-Sensors
+	public static final int LeftDriveEncoderA = 0;
+	public static final int LeftDriveEncoderB = 0;
+	public static final int RightDriveEncoderA = 0;
+	public static final int RightDriveEncoderB = 0;
+	
+}
