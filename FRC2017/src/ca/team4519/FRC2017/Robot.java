3bffa7a10048b4148502eb9b825c0b1314f9bcb4
CAdamz <connordgadams@gmail.com>
2017-01-16 19:53:06 -0500

    Adding universal defaults

--- FRC2017/src/ca/team4519/FRC2017/Robot.java
+++ FRC2017/src/ca/team4519/FRC2017/Robot.java
@@ -8,10 +8,17 @@ import ca.team4519.FRC2017.subsystems.GearBox;
 import ca.team4519.FRC2017.subsystems.Hopper;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.Scheduler;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 
 public class Robot extends IterativeRobot {
   
+	SendableChooser autoMode = new SendableChooser();
+	Command autoToExecute;
+	
 	Drivebase driveBase = new Drivebase();
 	Shooter flywheel = new Shooter();
 	Climber muscles = new Climber();
@@ -21,6 +28,14 @@ public class Robot extends IterativeRobot {
 	
     public void robotInit() {
        
+    	
+    	autoMode.addObject("Cross Baseline", null);
+    	autoMode.addObject("Hang Gear Right", null);
+    	autoMode.addObject("Hang Gear Center", null);
+    	autoMode.addObject("Hang Gear Left", null);
+    	autoMode.addObject("Red-Alliance: Gear+Shoot", null);
+    	autoMode.addObject("Blue-Alliance: Gear+Shoot", null);
+    	SmartDashboard.putData("Autonomous Mode Selector", autoMode);
     }
     
     public void updateAll() {
@@ -28,11 +43,13 @@ public class Robot extends IterativeRobot {
     }
     
     public void autonomousInit() {
-
+    	
+    	autoToExecute = (Command) autoMode.getSelected();
+    	autoToExecute.start();
     }
 
     public void autonomousPeriodic() {
-    
+    	Scheduler.getInstance().run();
     }
 
     public void teleopPeriodic() {
