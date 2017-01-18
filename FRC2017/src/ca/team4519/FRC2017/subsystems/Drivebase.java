5208db62419ebf3dbcaaadff42aae0551a56d7aa
CAdamz <connordgadams@gmail.com>
2017-01-17 19:22:40 -0500

    Adding defaults

--- FRC2017/src/ca/team4519/FRC2017/subsystems/Drivebase.java
+++ FRC2017/src/ca/team4519/FRC2017/subsystems/Drivebase.java
@@ -1,18 +1,27 @@
 package ca.team4519.FRC2017.subsystems;
 
 import ca.team4519.FRC2017.Constants;
+import ca.team4519.lib.Subsystem;
 
 import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 
-public class Drivebase {
+public class Drivebase extends Subsystem{
 
 	VictorSP leftDriveA;
 	VictorSP leftDriveB;
 	VictorSP rightDriveA;
 	VictorSP rightDriveB;
-
-	Encoder leftDriveEncodder;
+	
+	double leftPower;
+	double rightPower;
+	
+	
+	ADXRS450_Gyro gyro;
+	
+	Encoder leftDriveEncoder;
 	Encoder rightDriveEncoder;
 	
 	public void drivebaseInit() {
@@ -21,11 +30,46 @@ public class Drivebase {
 		rightDriveA = new VictorSP(Constants.rightDriveA);
 		rightDriveB = new VictorSP(Constants.rightDriveB);
 		
-		leftDriveEncodder = new Encoder(Constants.leftDriveEncoderA, Constants.leftDriveEncoderB, false);
+		gyro = new ADXRS450_Gyro();
+		
+		leftDriveEncoder = new Encoder(Constants.leftDriveEncoderA, Constants.leftDriveEncoderB, false);
 		rightDriveEncoder = new Encoder(Constants.rightDriveEncoderA, Constants.rightDriveEncoderB, false);
+		
+		gyro.calibrate();
 	}
 	
-	public void resetState() {
+
+	public void resetSensors() {
+		leftDriveEncoder.reset();
+		rightDriveEncoder.reset();
+		gyro.reset();
+		
+	}
+
+	public void arcadeDriveMath(double forwardAxis, double turningAxis) {
+ 		
+		leftPower =(-(forwardAxis - turningAxis));
+		rightPower =(forwardAxis + turningAxis);		
+	}
+	
+	public void setLeftRightPower(double leftOutput, double rightOutput) {
+		
+		leftDriveA.set(leftOutput);
+		leftDriveB.set(leftOutput);
+		rightDriveA.set(rightOutput);
+		rightDriveB.set(rightOutput);
+	}
+	
+	public void killAll() {
+		
+		leftPower = 0;
+		rightPower = 0;
+		
+	}
+
+	public void update() {
+		
+		SmartDashboard.putNumber("Gyro Angle", gyro.getAngle());
 		
 	}
 	
