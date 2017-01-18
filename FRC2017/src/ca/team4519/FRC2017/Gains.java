c32fe87d511fb7b6d7450168bd217113cf00bd7c
CAdamz <connordgadams@gmail.com>
2017-01-17 19:22:25 -0500

    Created File

---
+++ FRC2017/src/ca/team4519/FRC2017/Gains.java
@@ -0,0 +1,28 @@
+package ca.team4519.FRC2017;
+
+public class Gains {
+
+	public static final class Drive {
+		private Drive() {}
+			
+		public static double TicksPerFoot = 0.0;
+		
+		public static double LeftDistPID_P = 0.0;
+		public static double LeftDistPID_I = 0.0;
+		public static double LeftDistPID_D = 0.0;
+		public static double LeftVelPID_P = 0.0;
+		public static double LeftVelPID_I = 0.0;
+		public static double LeftVelPID_D = 0.0;
+		
+		public static double RightDistPID_P = 0.0;
+		public static double RightDistPID_I = 0.0;
+		public static double RightDistPID_D = 0.0;
+		public static double RightVelPID_P = 0.0;
+		public static double RightVelPID_I = 0.0;
+		public static double RightVelPID_D = 0.0;
+		
+			
+		
+	}
+	
+}
