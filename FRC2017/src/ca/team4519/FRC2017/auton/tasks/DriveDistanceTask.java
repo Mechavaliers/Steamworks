6c847df12a4344474a31b8f5bd5aae59e6ad9729
CAdamz <connordgadams@gmail.com>
2017-01-20 02:30:51 -0500

    Tried to start this

--- FRC2017/src/ca/team4519/FRC2017/auton/tasks/DriveDistanceTask.java
+++ FRC2017/src/ca/team4519/FRC2017/auton/tasks/DriveDistanceTask.java
@@ -1,5 +1,39 @@
 package ca.team4519.FRC2017.auton.tasks;
 
-public class DriveDistanceTask {
+public class DriveDistanceTask implements Task {
 
+	protected double distToGo;
+	protected double maxVel;
+	
+	public DriveDistanceTask(double distance, double maxVelocity, String name){
+		this.distToGo = distance;
+		this.maxVel = maxVelocity;
+	}
+
+
+	public boolean isTaskComplete() {
+		
+		return false;
+	}
+
+	@Override
+	public void exitTask() {
+
+		
+	}
+
+	@Override
+	public void output() {
+		
+		
+	}
+
+	@Override
+	public void runTask() {
+		
+		
+	}
+	
+	
+	
 }
