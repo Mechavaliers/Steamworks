b00eecadcade862c57221e9988d7fed636176470
Connor-Adams <connor.adams@uoit.net>
2017-01-20 21:13:49 -0500

    Tired to start but forgot how

--- FRC2017/src/ca/team4519/FRC2017/auton/tasks/DriveDistanceTask.java
+++ FRC2017/src/ca/team4519/FRC2017/auton/tasks/DriveDistanceTask.java
@@ -1,9 +1,12 @@
 package ca.team4519.FRC2017.auton.tasks;
 
+import ca.team4519.FRC2017.subsystems.Drivebase;
+
 public class DriveDistanceTask implements Task {
 
 	protected double distToGo;
 	protected double maxVel;
+	
 	
 	public DriveDistanceTask(double distance, double maxVelocity, String name){
 		this.distToGo = distance;
@@ -30,6 +33,7 @@ public class DriveDistanceTask implements Task {
 
 	@Override
 	public void runTask() {
+		Drivebase.grabInstance().setLeftRightPower(0, 0); //THIS IS TEMP PLZ CHANGE 
 		
 		
 	}
