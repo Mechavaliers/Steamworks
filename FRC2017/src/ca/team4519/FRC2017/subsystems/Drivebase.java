2c2464fcf735bf655d510a404905b219f77018c2
Connor <connor.adams@uoit.net>
2017-02-01 21:29:02 -0500

    Implemented trajectory controllers

--- FRC2017/src/ca/team4519/FRC2017/subsystems/Drivebase.java
+++ FRC2017/src/ca/team4519/FRC2017/subsystems/Drivebase.java
@@ -1,7 +1,12 @@
 package ca.team4519.FRC2017.subsystems;
 
 import ca.team4519.FRC2017.Constants;
+import ca.team4519.FRC2017.Gains;
 import ca.team4519.lib.Subsystem;
+import ca.team4519.lib.DrivetrainOutput;
+import ca.team4519.lib.RobotPose;
+import ca.team4519.FRC2017.subsystems.controllers.DriveLineController;
+
 import edu.wpi.first.wpilibj.VictorSP;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.Encoder;
@@ -15,11 +20,20 @@ public class Drivebase extends Subsystem{
 		return thisInstance;
 	}
 	
+	public interface Controllers {
+		DrivetrainOutput update(RobotPose pose);
+	}
+	
 	private final VictorSP leftDriveMotor, rightDriveMotor;
 	Encoder leftDriveEncoder, rightDriveEncoder;
 	ADXRS450_Gyro gyro;
 	
+	private Controllers controller = null;
+	
 	double leftPower, rightPower;
+	protected final double inchesPerTick = Gains.Drive.WheelSize_Inches * Math.PI / Gains.Drive.EncoderTicksPerRev;
+	
+	private RobotPose storedPose = new RobotPose(0, 0, 0, 0, 0, 0);
 	
 	public Drivebase() {
 		leftDriveMotor = new VictorSP(Constants.leftDrivePWM);
@@ -29,8 +43,19 @@ public class Drivebase extends Subsystem{
 		
 		leftDriveEncoder = new Encoder(Constants.leftDriveEncoderA, Constants.leftDriveEncoderB, false);
 		rightDriveEncoder = new Encoder(Constants.rightDriveEncoderA, Constants.rightDriveEncoderB, false);
+		leftDriveEncoder.setDistancePerPulse(inchesPerTick);
+		rightDriveEncoder.setDistancePerPulse(inchesPerTick);
 		
 		gyro.calibrate();
+	}
+	
+	public void setDistanceTarget(double distance, double velocity){
+		double whatVelocity = Math.min(Gains.Drive.ROBOT_MAX_VELOCITY, Math.max(velocity, 0));
+		controller = new DriveLineController(getRobotPose(), distance, whatVelocity);
+	}
+	
+	public void setDistanceTarget(double distance){
+		setDistanceTarget(distance,Gains.Drive.ROBOT_MAX_VELOCITY);
 	}
 	
 	public void resetSensors() {
@@ -62,12 +87,30 @@ public class Drivebase extends Subsystem{
 		rightPower =(forwardAxis + turningAxis);		
 	}
 	
-	public void setLeftRightPower(double leftOutput, double rightOutput) {
+	public void setDrivePower(DrivetrainOutput output) {
 		
-		leftDriveMotor.set(leftOutput);
-		rightDriveMotor.set(rightOutput);
+		leftDriveMotor.set(output.leftOutput);
+		rightDriveMotor.set(output.rightOutput);
 	}
 	
+	public void controlLoops(){
+		if(controller == null){
+			return;
+		}
+		setDrivePower(controller.update(getRobotPose()));
+	}
+	
+	public RobotPose getRobotPose(){
+		storedPose.reset(
+				leftDriveEncoder.getDistance(),
+				leftDriveEncoder.getDistance(),
+				leftDriveEncoder.getRate(),
+				rightDriveEncoder.getRate(),
+				gyro.getAngle(),
+				gyro.getRate());
+		return storedPose;
+		
+	}
 	
 	public void disableSubsystem() {
 		
