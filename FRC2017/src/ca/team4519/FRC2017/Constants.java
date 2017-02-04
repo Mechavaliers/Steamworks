9e75660c5db98e8b7e8a6f7c0dabf7ab244115cf
Connor-Adams <connor.adams@uoit.net>
2017-02-04 14:50:26 -0500

    changed from quadature encoders to single channels

--- FRC2017/src/ca/team4519/FRC2017/Constants.java
+++ FRC2017/src/ca/team4519/FRC2017/Constants.java
@@ -5,8 +5,6 @@ public class Constants {
 	/*
 	 * AS OF INITAL COMMIT ALL VALUES ARE 0 AND ONLY ACT AS PLACEHOLDERS
 	 */
-
-	
 	
 	//Controlls
 	public static final int gamepad = 0;
@@ -21,9 +19,7 @@ public class Constants {
 	
 	//Drivebase-Sensors
 	public static final int leftDriveEncoderA = 0;
-	public static final int leftDriveEncoderB = 0;
 	public static final int rightDriveEncoderA = 0;
-	public static final int rightDriveEncoderB = 0;
 	
 	//Shooter Sensors
 	public static final int leftFlywheelEncoder = 0;
