99a07cceeddee8232289877f6ae79f0214141cff
CAdamz <connordgadams@gmail.com>
2017-02-15 22:16:50 -0500

    Revert "Actually did something with auto"
    This reverts commit 97bdabf2df4bae3f2838458c4c0f79f3dc4880f5.

--- FRC2017/src/ca/team4519/FRC2017/auton/modes/LaneAGear.java
+++ FRC2017/src/ca/team4519/FRC2017/auton/modes/LaneAGear.java
@@ -1,14 +1,13 @@
 package ca.team4519.FRC2017.auton.modes;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
-import ca.team4519.FRC2017.auton.commands.WaitForDriveSetpointCommand;
-import ca.team4519.FRC2017.auton.commands.WaitForTurnCommand;
-import ca.team4519.FRC2017.subsystems.Drivebase;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 
 
-
+/**
+ *
+ */
 public class LaneAGear extends CommandGroup {
 
 	DriverStation ds;
@@ -32,15 +31,7 @@ public class LaneAGear extends CommandGroup {
     	getAlliance();
     	
     	if(red){
-    		Drivebase.grabInstance().setDistanceTarget(50);
-    		new WaitForDriveSetpointCommand(50, true, 5);
-    		Drivebase.grabInstance().setTurnTarget(45);
-    		new WaitForTurnCommand(45, true, 3);
-    		Drivebase.grabInstance().setDistanceTarget(25);
-    		new WaitForDriveSetpointCommand(25, true, 5);
-    		Drivebase.grabInstance().setReverseDistanceTarget(5);
-    		new WaitForDriveSetpointCommand(5, false, 1);
-    		Drivebase.grabInstance().setTurnTarget(10);
+    		
     	}else if (blue){
     		
     	}else{
