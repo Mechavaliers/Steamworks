b36912f410a60b5da26d34ae9c611be372cc4cf6
Connor <connor.adams@uoit.net>
2017-03-07 22:42:40 -0500

    Changed shooter logic

--- FRC2017/src/ca/team4519/FRC2017/Robot.java
+++ FRC2017/src/ca/team4519/FRC2017/Robot.java
@@ -18,33 +18,36 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends MechaRobotBase{
 	
-	MultiThreader controlLoops = new MultiThreader("100Hz", 1.0/100.0);
+	MultiThreader autonLoop = new MultiThreader("100Hz", 1.0/100.0);
+	MultiThreader fastLoop = new MultiThreader("200Hz" , 1.0/200.0);
 	
 	AutonRunner autonLoopRunner = new AutonRunner();
 	
 	SendableChooser<AutoMode> auton = new SendableChooser<AutoMode>();
+	
 	Joystick PS4 = new Joystick(0);
 	Joystick OP = new Joystick(1);
 
 	public void robotInit() {
        
-		controlLoops.addThread(Drivebase.grabInstance());
+		autonLoop.addThread(Drivebase.grabInstance());
+		fastLoop.addThread(Shooter.grabInstance());
 		
-		auton.addObject("Red: Lane 1 Gear", null);
-		auton.addObject("Red: Lane 1 Gear + Hopper", null);
-		auton.addObject("Red: Lane 1 Gear + Boiler", null);
-		auton.addObject("Red: Lane 1 Hopper + Boiler", null);
-		auton.addObject("Red: Lane 2 Gear -> Left", null);
-		auton.addObject("Red: Lane 2 Gear -> Right", null);
-		auton.addObject("Red: Lane 3 Gear", null);
+		auton.addObject("Red: Lane 1 Gear", new R1Gear());
+		auton.addObject("Red: Lane 1 Gear + Hopper", new R1GearAndHopper());
+		auton.addObject("Red: Lane 1 Gear + Boiler", new R1GearAndBoiler());
+		auton.addObject("Red: Lane 1 Hopper + Boiler", new R1HopperAndBoiler());
+		auton.addObject("Red: Lane 2 Gear -> Left", new R2GearGoLeft());
+		auton.addObject("Red: Lane 2 Gear -> Right", new R2GearGoRight());
+		auton.addObject("Red: Lane 3 Gear", new R3Gear());
 		
-		auton.addObject("Blue: Lane 1 Gear", null);
-		auton.addObject("Blue: Lane 2 Gear -> Left", null);
-		auton.addObject("Blue: Lane 2 Gear -> Right", null);
-		auton.addObject("Blue: Lane 3 Gear", null);
-		auton.addObject("Blue: Lane 3 Gear + Hopper", null);
-		auton.addObject("Blue: Lane 3 Gear + Boiler", null);
-		auton.addObject("Blue: Lane 3 Hopper + Boiler", null);
+		auton.addObject("Blue: Lane 1 Gear", new B1Gear());
+		auton.addObject("Blue: Lane 2 Gear -> Left", new B2GearGoLeft());
+		auton.addObject("Blue: Lane 2 Gear -> Right", new B2GearGoRight());
+		auton.addObject("Blue: Lane 3 Gear", new B3Gear());
+		auton.addObject("Blue: Lane 3 Gear + Hopper", new B3GearAndHopper());
+		auton.addObject("Blue: Lane 3 Gear + Boiler", new B3GearAndBoiler());
+		auton.addObject("Blue: Lane 3 Hopper + Boiler", new B3HopperAndBoiler());
 
 		auton.addDefault("Lane B Gear", new LaneBGear());
 		auton.addObject("Lane B Go Right", new LaneBGoRight());
@@ -63,7 +66,7 @@ public class Robot extends MechaRobotBase{
     	
     	autonLoopRunner.selectAuto(mode);
     	
-    	controlLoops.start();
+    	autonLoop.start();
     	mode.init();
     	autonLoopRunner.start();
     }
@@ -75,7 +78,7 @@ public class Robot extends MechaRobotBase{
     	Scheduler.getInstance().disable();
     	Drivebase.grabInstance().getRobotPose();
     	Drivebase.grabInstance().resetSensors();
-    	//controlLoops.start();
+    	fastLoop.start();
     
     }
     
@@ -84,7 +87,7 @@ public class Robot extends MechaRobotBase{
        Drivebase.grabInstance().setDrivePower(Drivebase.grabInstance().arcadeDriveMath(PS4.getRawAxis(1), PS4.getRawAxis(4), PS4.getRawButton(6)));
        Hopper.grabInstance().hopperControl(OP.getRawButton(1), OP.getRawButton(3));
        Climber.grabInstance().climb(OP.getRawButton(4), OP.getRawAxis(1));
-       Shooter.grabInstance().test(OP.getRawButton(1));
+       Shooter.grabInstance().toggleState(OP.getRawButton(1));
        GearBox.grabInstance().setDeg(PS4.getRawButton(4), PS4.getRawButton(1));
       
 
@@ -92,12 +95,14 @@ public class Robot extends MechaRobotBase{
 
     
     public void disabledInit() {
-    	controlLoops.stop();
     	Drivebase.grabInstance().disableSubsystem();
     	Climber.grabInstance().disableSubsystem();
     	GearBox.grabInstance().disableSubsystem();
     	Hopper.grabInstance().disableSubsystem();
     	Shooter.grabInstance().disableSubsystem();
+    	
+    	autonLoop.stop();
+    	fastLoop.stop();
     }
     
     public void allPeriodic() {
--- FRC2017/src/ca/team4519/FRC2017/subsystems/Shooter.java
+++ FRC2017/src/ca/team4519/FRC2017/subsystems/Shooter.java
@@ -3,14 +3,11 @@ package ca.team4519.FRC2017.subsystems;
 import ca.team4519.lib.Subsystem;
 import ca.team4519.lib.Thread;
 import ca.team4519.FRC2017.Constants;
-import ca.team4519.FRC2017.Gains;
-import ca.team4519.lib.pid.FlywheelController;
 
-import edu.wpi.first.wpilibj.Counter;
 import edu.wpi.first.wpilibj.Talon;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Shooter extends Subsystem implements Thread{
+
 
 	private static Shooter thisInstance = new Shooter();
 	
@@ -18,103 +15,78 @@ public class Shooter extends Subsystem implements Thread{
 
 	boolean yay = false;
 	boolean go = false;
-	
-	public Counter Flywheel_sensor;
+
 	
 	public enum Flywheel_State{
-		OFF, KEY, WALL
+		OFF, ON, 
 	}
+	
+	public Flywheel_State flywheelState = Flywheel_State.OFF;
 	
 	public Shooter(){
 		thisInstance = this;
 		
-		Flywheel = new Talon(Constants.FlywheelPWM);
-		Flywheel_sensor = new Counter(Constants.FlywheelEncoder);
-		
+		Flywheel = new Talon(Constants.FlywheelPWM);		
 	}
-	
-	public FlywheelController Flywheel_Controller = new FlywheelController(
-			Flywheel_sensor,
-			Gains.Flywheel.RPM_P, 
-			Gains.Flywheel.RPM_I, 
-			Gains.Flywheel.RPM_D, 
-			Gains.Flywheel.RPM_F, 
-			Gains.Flywheel.TicksPerRev, 
-			Gains.Flywheel.RPM_Deadband);
-
 	
 	public static Shooter grabInstance() {
 		return thisInstance;
 	}
+
 	
-	public void setRPMTarget(double target){
-		Flywheel_Controller.setRPM(target);
-	}
-	
-	public void enableFlywheel() {
-		Flywheel.set(Flywheel_Controller.controllerOutput());
-	
-	}
-	
-	public void test(boolean test){
-		if(!test){
+	public void toggleState(boolean toggleState){
+				
+		if(!toggleState){
 			go = true;
 		}else if (go){
-			yay = !yay;
+			if(flywheelState == Flywheel_State.OFF){
+				flywheelState = Flywheel_State.ON;
+			}else if(flywheelState == Flywheel_State.ON){
+				flywheelState = Flywheel_State.OFF;
+			}
 			go = false;
 		}
-		
-		Flywheel.set(yay? 0.60:0.0);
-		
 	}
+
 		
-	public void Flywheel_State_Machine(Flywheel_State state){
+	public void Flywheel_State_Machine(){
 		
-		switch(state){
-			case WALL:
-				setRPMTarget(Gains.Flywheel.RPM_TARGET_WALL);
-				enableFlywheel();
-				break;
-			case KEY:
-				setRPMTarget(Gains.Flywheel.RPM_TARGET_KEY);
-				enableFlywheel();
-				break;
+		switch(flywheelState){
+			case ON:
+				Flywheel.set(0.60);
 			case OFF:
+				Flywheel.set(0.0);				
 			default:
-				Flywheel_Controller.disable();
-				break;
+				Flywheel.set(0.0);
 		}	
 	}
 	
-	
-	public double getFlywheelVelocity(){
-		return (Flywheel_sensor.getRate() / Gains.Flywheel.TicksPerRev) / 60;
-	}
-	
-	public double getFlywheelRevsPerMin(){
-		return getFlywheelVelocity();
-	}
-
-	public void resetSensors() {
-		Flywheel_sensor.reset();	
-		
+	public void resetSensors() {	
 	}
 
 	public void update() {
-	//	SmartDashboard.putBoolean("FlyWheel Controller Status", Flywheel_Controller.isEnabled());
-	//	SmartDashboard.putNumber("Flywheel RPM", getFlywheelVelocity());
-	//	SmartDashboard.putBoolean("Flywheel on Target?", Flywheel_Controller.inRange());
 	}
 
 
 	public void disableSubsystem() {
-		Flywheel_Controller.disable();
+		flywheelState = Flywheel_State.OFF;
+		Flywheel.set(0.0);
+		
 	}
 
 	@Override
 	public void controlLoops() {
-		
-		
+		switch(flywheelState){
+		case ON:
+			Flywheel.set(0.60);
+			break;
+		case OFF:
+			Flywheel.set(0.0);
+			break;
+		default:
+			Flywheel.set(0.0);
+			break;
+	}
 	}
 
 }
