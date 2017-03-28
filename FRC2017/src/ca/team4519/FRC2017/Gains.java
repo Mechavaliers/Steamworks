bf49b493ed6672663cc7ce2177d8c7065806ad44
Connor-Adams <connor.adams@uoit.net>
2017-03-27 22:48:31 -0400

    Changed spacing and removed unnecessary comments

--- FRC2017/src/ca/team4519/FRC2017/Gains.java
+++ FRC2017/src/ca/team4519/FRC2017/Gains.java
@@ -8,13 +8,12 @@ public class Gains {
 		public static double CONTROL_LOOP_TIME = 0.01;
 		
 		public static double HANDLING_MODIFIER = 1.0;
-		
 		public static double PATH_TOLLERANCE = 0.25;
 		
-		public static double ROBOT_MAX_VELOCITY= 100.0;
+		public static double ROBOT_MAX_VELOCITY= 100.0; //TODO Update this at Mac
 		public static double ROBOT_MAX_ACCELERATION = 80.0;
-		public static double ROBOT_MAX_ROTATIONAL_VELOCITY = 250.0; //75 for some reason 35 is here but im trying 25
-		public static double ROBOT_MAX_ROTATIONAL_ACCELERATION = 250.0; //these are the same, read above. We turn faster then 35 or 75 degs/s
+		public static double ROBOT_MAX_ROTATIONAL_VELOCITY = 250.0;
+		public static double ROBOT_MAX_ROTATIONAL_ACCELERATION = 250.0; 
 		public static double Wheelbase_Width = 35.25;
 		public static double Wheelbase_Length = 39.25; 
 		public static double EncoderDegsPerRev = 360.0;
